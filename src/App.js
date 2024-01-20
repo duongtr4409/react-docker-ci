995f69a981358646ff96dd1e09897cfcefacc220
DuownG <duongnv.ptit@gmail.com>
2024-01-20 22:09:43 +0700

    fix app.js to run pass unit test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn
+          Learn React
         </a>
         <h1>DuowngToraaa</h1>
       </header>
